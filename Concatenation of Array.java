2d76077eef953ab7814f55022ceb541f6d3fb936
JEYAKUMAR3104 <jebakumar717@gmail.com>
2026-03-10 14:27:45 +0530

    Created Concatenation of Array.java
    https://neetcode.io/problems/concatenation-of-array/question

---
+++ Concatenation of Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] num1=new int [nums.length*2];
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            num1[k]=nums[i];
+            k++;
+        }
+        for(int i=0;i<nums.length;i++){
+            num1[k]=nums[i];
+            k++;
+        }
+        return num1;
+        
+    }
+}
+
+//check this link: https://neetcode.io/problems/concatenation-of-array/history?list=allNC&submissionIndex=2
